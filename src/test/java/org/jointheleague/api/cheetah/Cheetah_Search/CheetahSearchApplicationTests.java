82440c3cbb6338a57ab1ba8bf585c1b4d4f45d27
mjfre <mjfre@aol.com>
2021-02-02 22:11:01 -0800

    initial

---
+++ src/test/java/org/jointheleague/api/cheetah/Cheetah_Search/CheetahSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jointheleague.api.cheetah.Cheetah_Search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheetahSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
